e616043ad1bde5dd7511cc8a83afc5eb4dc81f97
CodeWithRasool <srasool6371@gmail.com>
2025-07-29 12:31:04 +0530

    lambda

---
+++ Classwork/cwd21.1.lambdafunctions-square.py
@@ -0,0 +1,3 @@
+n = int(input())  # takes an integer input from the user
+square_lambda = lambda n: n * n  # defines an anonymous (lambda) function
+print('square:', square_lambda(n))  # calls the lambda function and prints the square
