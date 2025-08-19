a555f1c1f24a7b5e3116b048da7ae49db27ea0ae
CodeWithRasool <srasool6371@gmail.com>
2025-08-19 12:01:30 +0530

    poly

---
+++ Classwork/cwd29.1.methodoverriding.py
@@ -0,0 +1,43 @@
+class NormalUser():
+    def __init__(self,username):
+        self.username=username
+        print('\nWelcome to youtube.. explore our content..')
+
+    def playvedio(self):
+        print('Ads  Included')
+        print('No Background Play')
+        print('Download with Low Quality')
+        print('No yt music')
+    def profile(self):
+        print('You can upload your profile')
+    def MultipleDevice(self):
+        print('you can login with different devices')
+    def shorts(self):
+        print('You can see the yt shorts')
+    def likecommentshare(self):
+        print('You can like share and comment')
+
+class PremiumUser(NormalUser):
+    def __init__(self,username):
+        super().__init__(username)
+
+    def playvedio(self):
+        print('Ads Free')
+        print('You can play on Background')
+        print('Exclusive content')
+        print('Download with hign Quality')
+        print('YT music is available')
+
+hemanth=NormalUser('hemanth')
+hemanth.playvedio()
+hemanth.profile()
+hemanth.shorts()
+hemanth.MultipleDevice()
+hemanth.likecommentshare()
+
+shesu=PremiumUser('Sheshu')
+shesu.playvedio()
+shesu.profile()
+shesu.shorts()
+shesu.MultipleDevice()
+shesu.likecommentshare()
